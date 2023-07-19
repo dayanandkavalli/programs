7ab0d8d7f3f389d14be9ad67a2ccd4f5f1d24fa5
DAYANAND KAVALLI <67493031+dayanandkavalli@users.noreply.github.com>
2023-07-19 09:38:50 +0530

    Update ASCII_value.py

--- ASCII_value.py
+++ ASCII_value.py
@@ -3,29 +3,26 @@ def transform_string(s):
     transformed_string = list(s)
     length = len(s)
     for i in range(length):
-        ascii_val = ord(s[i])  
+        ascii_val = ord(s[i])
 
-        if ascii_val % 2 == 0:  
-            if i < length - 1 and not transformed_string[i+1].isdigit():
-                next_ascii_val = (ascii_val % 7) + ord(s[i+1]) 
+        if ascii_val % 2 == 0:
+            if i < length - 1 and not transformed_string[i + 1].isdigit():
+                next_ascii_val = (ascii_val % 7) + ord(s[i + 1])
                 if next_ascii_val > 127:
                     next_ascii_val = 83
-                transformed_string[i+1] = chr(next_ascii_val)
+                transformed_string[i + 1] = chr(next_ascii_val)
         else:
-            if i > 0 and not transformed_string[i-1].isdigit():
-                prev_ascii_val = ord(transformed_string[i-1]) - (ascii_val % 5) 
+            if i > 0 and not transformed_string[i - 1].isdigit():
+                prev_ascii_val = ord(transformed_string[i - 1]) - (ascii_val % 5)
                 if prev_ascii_val < 0:
                     prev_ascii_val = 83
-                transformed_string[i-1] = chr(prev_ascii_val)
+                transformed_string[i - 1] = chr(prev_ascii_val)
 
         ascii_values.append(str(ascii_val))
 
     transformed_string = "".join(transformed_string)
     return transformed_string, "-".join(ascii_values)
-
-
-# Example usage
-s = "sHQen}"
+s = input("Enter a string: ")
 transformed_string, ascii_values = transform_string(s)
 print("Input string:", s)
 print("ASCII values:", ascii_values)
